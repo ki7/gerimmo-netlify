b490f547e2316521e90d213e8801e7a810b32cdb
ki7 <assym.el.mahdy@gmail.com>
2019-01-25 19:15:16 +0100

    Rollback undefined pages <= local cms?

--- src/pages/Appartements.js
+++ src/pages/Appartements.js
@@ -1,1 +1,12 @@
-undefined
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default class Appartements extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section className="section" />
+      </Layout>
+    );
+  }
+}
--- src/pages/Partenaires.js
+++ src/pages/Partenaires.js
@@ -1,1 +1,16 @@
-undefined
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default class Partenaires extends React.Component {
+  render() {
+
+
+    return (
+      <Layout>
+        <section className="section">
+       Yolo
+        </section>
+      </Layout>
+    );
+  }
+}
