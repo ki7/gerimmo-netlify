befe9c2f63d31c3d545945807ea1e2d35b5ec8e5
Assym <assym.el.mahdy@gmail.com>
2019-01-25 18:07:43 +0000

    Update Pages “partenaires”

--- src/pages/Partenaires.js
+++ src/pages/Partenaires.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import Layout from '../components/Layout';
-
-export default class Partenaires extends React.Component {
-  render() {
-
-
-    return (
-      <Layout>
-        <section className="section">
-       Yolo
-        </section>
-      </Layout>
-    );
-  }
-}
+undefined
