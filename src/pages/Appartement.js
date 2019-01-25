f2aa101f8fa13d550c96f70ef4186870a54b57c3
ki7 <assym.el.mahdy@gmail.com>
2019-01-25 17:18:54 +0100

    ADD main pages, nav and basic styles

---
+++ src/pages/Appartement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default class Appartement extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section className="section" />
+      </Layout>
+    );
+  }
+}
---
+++ src/pages/Proprietaire.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default class Proprietaire extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section className="section">Yolo</section>
+      </Layout>
+    );
+  }
+}
